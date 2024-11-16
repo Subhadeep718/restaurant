19f34a0a7de1ad1f3f764c38c47451a9efc3fb2b
Subhadeep Samanta <subhadeep.99graphicdesigner@gmail.com>
2024-11-16 12:54:44 +0530

    Add files via upload

---
+++ assets/JS/main.js
@@ -0,0 +1,19 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  autoplayTimeout: 3000,
+  dots: true,
+  //   nav: true,
+  autoplay: true,
+  items: 1,
+  //   responsive: {
+  //     0: {
+  //       items: 1,
+  //     },
+  //     600: {
+  //       items: 1,
+  //     },
+  //     1000: {
+  //       items: 1,
+  //     },
+  //   },
+});
